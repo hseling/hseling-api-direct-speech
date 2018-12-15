6237ebc3539824e0211b80b3594816169ea401b6
lilaspourpre <irina.nikishina@mail.ru>
2018-12-15 18:06:25 +0300

    add statistics

--- hseling_api_direct_speech/query.py
+++ hseling_api_direct_speech/query.py
@@ -18,18 +18,24 @@ def query_data(query_type, contents, tags):
         return {"error": "incorrect query type"}
 
 
-def get_tags_from(tree, tag):
-    tag = tag["tag"]
+def get_tags_from(tree, tag_with_param):
+    tag = tag_with_param["tag"]
     result = {}
-    if "param" in tag:
-        result[tag] = [i.text for i in tree.findAll(tag, tag["param"])]
+    if "param" in tag_with_param:
+        return [i.text for i in tree.findAll(tag, tag_with_param["param"])]
     else:
-        result[tag] = [i.text for i in tree.findAll(tag)]
+        return [i.text for i in tree.findAll(tag)]
     return result
 
 
 def get_statistics(tree):
-    return {"statistics": 0}
+    result = {}
+    tags_with_params = {"speech":[], "said":["type", "aloud"], "author_comment":[], "speech_verb":["semantic", "emotion"]}
+    for tag in tags_with_params:
+        result[tag] = len(tree.findAll(tag))
+        for param in tags_with_params[tag]:
+            result[tag+"_"+param] = len(tree.findAll(tag, ))
+    return result
 
 
 def get_examples(tree, tags):
@@ -42,3 +48,8 @@ def get_examples(tree, tags):
 def read_xml(text):
     return BeautifulSoup('<text>'+text+'</text>', "lxml")
 
+filepath = "C:/Users/Irina/Downloads/PortableGit/hseling-api-direct-speech/app/static/gold.txt"
+with open(filepath, "r", encoding="utf-8") as r:
+    text = r.read()
+tree = read_xml(text)
+print(len(get_tags_from(tree, {"tag":"speech_verb", "param":{"characteristics": "neutral"}})))
