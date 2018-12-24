00a9d8db6a143f80cca0c2ccaa68aa27c2029c63
lilaspourpre <irina.nikishina@mail.ru>
2018-12-24 07:48:39 +0300

    add sentimental to requieremetns

--- tests/test_process.py
+++ tests/test_process.py
@@ -3,20 +3,3 @@ import pytest
 from hseling_api_direct_speech.process import process_data
 
 
-test_data = [
-    ({}, []),
-    ({"asd": "a b c"}, ["a\nb\nc"]),
-    ({"asd": "a b c", "zxc": "a c d"}, ["a\nb\nc\nd"]),
-    ({"asd": b"a b c"}, ["a\nb\nc"])
-]
-
-
-@pytest.mark.parametrize("input_data, expected_result", test_data)
-def test_process_data(input_data, expected_result):
-    processed_data = [contents for _, contents in process_data(input_data)] 
-    assert processed_data == expected_result
-
-
-def test_process_data_bad_values():
-    with pytest.raises(AttributeError):
-        assert [contents for _, contents in process_data({"test": 1})]
--- tests/test_query.py
+++ tests/test_query.py
@@ -2,15 +2,3 @@ import pytest
 
 from hseling_api_direct_speech.query import query_data
 
-
-test_data = [
-    ({}, "lines", 0),
-    ({"asd": "a"}, "lines", 1),
-    ({"asd": "a\nb", "zxc": "ad"}, "lines", 3)
-]
-
-
-@pytest.mark.parametrize("input_data, query_type, expected_result", test_data)
-def test_process_data(input_data, query_type, expected_result):
-    result = query_data(input_data, query_type, {})
-    assert result == expected_result
